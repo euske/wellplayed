6647191bba8ee037032ce460091c179ae9432768
Yusuke Shinyama <yusuke@shinyama.jp>
2017-04-03 00:23:37 +0900

    Pattern switching support.

--- src/game.ts
+++ src/game.ts
@@ -51,7 +51,7 @@ class Player extends Entity {
     }
 
     fire() {
-	var bullet = new Bullet(this.pos, new Rect(-1, -4, 2, 8));
+	let bullet = new Bullet(this.pos, new Rect(-1, -4, 2, 8));
 	bullet.movement = new Vec2(0, -8);
 	bullet.frame = this.scene.screen;
 	this.scene.add(bullet);
@@ -66,6 +66,47 @@ class Player extends Entity {
     }
 
     collidedWith(entity: Entity) {
+	if (entity instanceof Changer) {
+	    this.scene.changeBase(entity.pattern);
+	    entity.stop();
+	}
+    }
+}
+
+
+//  Changer
+//
+class Changer extends Projectile {
+
+    pattern: number;
+    
+    constructor(frame: Rect, pattern: number) {
+	let pos: Vec2;
+	let movement: Vec2;
+	switch (rnd(4)) {
+	case 0:
+	    pos = new Vec2(frame.x+rnd(frame.width), frame.y);
+	    movement = new Vec2(0, 1);
+	    break;
+	case 1:
+	    pos = new Vec2(frame.x, frame.y+rnd(frame.height));
+	    movement = new Vec2(1, 0);
+	    break;
+	case 2:
+	    pos = new Vec2(frame.x+rnd(frame.width), frame.bottom());
+	    movement = new Vec2(0, -1);
+	    break;
+	case 3:
+	    pos = new Vec2(frame.right(), frame.y+rnd(frame.height));
+	    movement = new Vec2(-1, 0);
+	    break;
+	}
+	super(pos);
+	this.frame = frame;
+	this.movement = movement.scale(1+rnd(2));
+	this.pattern = pattern;
+	this.sprite.imgsrc = SPRITES.get(pattern, 1);
+	this.collider = this.sprite.getBounds(new Vec2());
     }
 }
 
@@ -85,11 +126,15 @@ class Game extends GameScene {
     baseTime: number;
     toneTime: number;
     
+    baseDuration: number;
+    toneIndex: number;
     curBase: HTMLAudioElement;
-    curBaseDuration: number;
     curTone: HTMLAudioElement;
-    curToneIndex: number;
+    nextBase: HTMLAudioElement;
+    nextTone: HTMLAudioElement;
     
+    changeProb: number;
+
     init() {
 	super.init();
 	this.scoreBox = new TextBox(this.screen.inflate(-8,-8), FONT);
@@ -100,14 +145,35 @@ class Game extends GameScene {
 	this.score = 0;
 	this.updateScore();
 
-	this.curBaseDuration = TONE_LEN*16;
-	this.curBase = SOUNDS['base1'];
-	this.curTone = SOUNDS['tone1'];
-	this.curToneIndex = 0;
+	this.baseDuration = TONE_LEN*16;
+	this.toneIndex = 0;
+	this.curBase = this.nextBase = SOUNDS['base0'];
+	this.curTone = this.nextTone = null;
+	this.resetTimer();
 	
+	this.changeProb = 0;
+    }
+
+    resetTimer() {
 	this.curTime = 0;
 	this.baseTime = -999999;
 	this.toneTime = -999999;
+    }
+    
+    onBlur() {
+	if (this.curBase !== null) {
+	    this.curBase.pause();
+	}
+	if (this.curTone !== null) {
+	    this.curTone.pause();
+	}
+    }
+
+    onFocus() {
+	this.resetTimer();
+	if (this.curBase !== null) {
+	    this.curBase.play();
+	}
     }
 
     onButtonPressed(keysym: KeySym) {
@@ -128,28 +194,43 @@ class Game extends GameScene {
 	    this.curTime = t;
 	}
 	let dt = t - this.curTime;
-	if (this.curBase !== null) {
-	    if (this.baseTime + this.curBaseDuration <= dt) {
-		this.baseTime = Math.floor(dt/this.curBaseDuration) * this.curBaseDuration;
+	let baseChanged = false;
+	if (this.baseTime + this.baseDuration <= dt) {
+	    this.baseTime = Math.floor(dt/this.baseDuration) * this.baseDuration;
+	    if (this.curBase !== null) {
 		this.curBase.pause();
+	    }
+	    this.curBase = this.nextBase;
+	    if (this.curBase !== null) {
 		this.curBase.currentTime = MP3_GAP;
 		this.curBase.play();
 	    }
+	    baseChanged = true;
+	}
+	if (this.toneTime + TONE_LEN <= dt) {
+	    this.toneTime = Math.floor(dt/TONE_LEN) * TONE_LEN;
 	    if (this.curTone !== null) {
-		if (this.toneTime + TONE_LEN <= dt) {
-		    this.toneTime = Math.floor(dt/TONE_LEN) * TONE_LEN;
-		    this.curTone.pause();
-		    if (this.firing) {
-			let r = this.player.pos.x/this.screen.width;
-			let i = ((Math.random() < r)? rnd(10) :
-				 clamp(0, this.curToneIndex+rnd(3)-1, 9));
-			this.curTone.currentTime = (TONE_LEN*4*i)/1000 + MP3_GAP;
-			this.curTone.play();
-			this.curToneIndex = i;
-			this.player.fire();
-		    }
-		}
+		this.curTone.pause();
 	    }
+	    if (this.nextTone !== null && baseChanged) {
+		this.curTone = this.nextTone;
+		this.nextTone = null;
+	    }
+	    if (this.curTone !== null && this.firing) {
+		let r = this.player.pos.x/this.screen.width;
+		let i = ((Math.random() < r)? rnd(10) :
+			 clamp(0, this.toneIndex+rnd(3)-1, 9));
+		this.curTone.currentTime = (TONE_LEN*4*i)/1000 + MP3_GAP;
+		this.curTone.play();
+		this.toneIndex = i;
+		this.player.fire();
+	    }
+	}
+
+	this.changeProb += Math.random()*.05;
+	if (1.0 <= this.changeProb) {
+	    this.changeProb -= 1.0;
+	    this.add(new Changer(this.screen, 1+rnd(3)));
 	}
     }
 
@@ -163,5 +244,22 @@ class Game extends GameScene {
     updateScore() {
 	this.scoreBox.clear();
 	this.scoreBox.putText(['SCORE: '+this.score]);
+    }
+
+    changeBase(pattern: number) {
+	switch (pattern) {
+	case 1:
+	    this.nextBase = SOUNDS['base1'];
+	    this.nextTone = SOUNDS['tone1'];
+	    break;
+	case 2:
+	    this.nextBase = SOUNDS['base2'];
+	    this.nextTone = SOUNDS['tone2'];
+	    break;
+	case 3:
+	    this.nextBase = SOUNDS['base3'];
+	    this.nextTone = SOUNDS['tone3'];
+	    break;
+	}
     }
 }
